72f6504b38b905a862ee9d62fc581fd9905eb2d0
David <davidi.b23@gmail.com>
2018-08-30 18:43:07 +0200

    Nuevas slider, completada seccion de recetas y traduccion completa

--- js/mapa.js
+++ js/mapa.js
@@ -2,10 +2,16 @@ function mapaGoogle() {
 
   var marcadores = [];
   var localidades = [
-    ['<h5>Portonovo</h5> En la explanada del campo de Baltar (Lunes y Jueves)', 42.398968, -8.824580],
-    ['<h5>Dena</h5> Plaza de Abastos (Martes)', 42.449228, -8.814859],
-    ['<h5>Cambados</h5> Al lado de la plaza de Abastos (Miércoles y Sábado)', 42.513160, -8.816315],
-    ['<h5>O Grove</h5> Plaza de Abastos (Viernes)', 42.497416, -8.864399]
+    ['<h6>Portonovo</h6> <p>En la explanada del campo de Baltar (Lunes y Jueves 8h - 15h)</p>' +
+          '<a class="whoto" href="https://goo.gl/maps/RAhjwHUiBe72" target="_blank">Cómo llegar</a>', 42.398968, -8.824580],
+    ['<h6>Dena</h6> <p>Plaza de Abastos (Martes 8h - 15h)</p>' +
+          '<a class="whoto" href="https://goo.gl/maps/cq7unfNywap" target="_blank">Cómo llegar</a>', 42.449228, -8.814859],
+    ['<h6>Cambados</h6> <p>Al lado de la plaza de Abastos (Miércoles y Sábado 8h - 15h)</p>' +
+          '<a class="whoto" href="https://goo.gl/maps/adYSvaZ3Bn92" target="_blank">Cómo llegar</a>', 42.513160, -8.816315],
+    ['<h6>O Grove</h6> <p>Plaza de Abastos (Viernes 8h - 15h)</p>'+
+          '<a class="whoto" href="https://goo.gl/maps/u8hqfqNXiJC2" target="_blank">Cómo llegar</a>', 42.497416, -8.864399],
+    ['<h6>Viliquín</h6> <p>Almacén local - (Todos los días por la tarde)</p>'+
+          '<a class="whoto" href="https://goo.gl/maps/73BJ2RD2sE82" target="_blank">Cómo llegar</a>', 42.439606, -8.818628]
   ];
   
   var mapa = new google.maps.Map(document.getElementById("map-canvas"), {
